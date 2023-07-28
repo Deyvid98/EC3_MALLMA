cbde409f2e4c9fce6523c2249d075a78a1f18121
Deyvid98 <140784354+Deyvid98@users.noreply.github.com>
2023-07-28 18:11:58 +0000

    1

--- src/main/java/com/example/demo/Instrumento.java
+++ src/main/java/com/example/demo/Instrumento.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+/*import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.CascadeType;*/
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
